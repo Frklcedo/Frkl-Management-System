da7bb93cfab97045e93318f81a422e1021c23f0f
Frklcedo <franklinmacedo5115@gmail.com>
2023-12-22 00:02:22 -0300

    Models creation

---
+++ app/Models/Debt.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
+
+class Debt extends Model
+{
+    use HasFactory;
+
+    public function profile() : BelongsTo
+    {
+        return $this->belongsTo(Profile::class);
+    }
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function creditor() : HasOneThrough
+    {
+        return $this->hasOneThrough(Creditor::class, User::class);
+    }
+    public function installments() : HasMany
+}
